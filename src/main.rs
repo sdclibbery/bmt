7a851ea4524e76d5cd1f0149839588c374747d33
sdclibbery <sdclibbery@googlemail.com>
2019-06-11 22:45:01 +0100

    termion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use termion::raw::IntoRawMode;
+use termion::{color, style};
+use std::io::{Write, stdout};
+
 fn main() {
-    println!("Hello, world!");
+    // Enter raw mode.
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    // Write to stdout (note that we don't use `println!`)
+    writeln!(stdout, "{red}Hello {green}World{reset}\r",
+            red   = color::Fg(color::Red),
+            green = color::Fg(color::Green),
+            reset = style::Reset).unwrap();
+
+    // Here the destructor is automatically called, and the terminal state is restored.
 }
