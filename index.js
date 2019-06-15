786ececc3ffcc7a0968b07c35b86759385e2c78f
sdclibbery <sdclibbery@googlemail.com>
2019-06-15 21:49:55 +0100

    position data

--- index.js
+++ index.js
@@ -21,3 +21,24 @@ bitmex.request('GET', '/user/walletSummary', {  })
       console.log(`wallet ${transactType} ${walletBalance} ${currency}`)
     })
   }).catch(console.log)
+
+bitmex.request('GET', '/orderBook/L2', { symbol: symbol, depth: 1 })
+  .then(([o1,o2]) => {
+    console.log(`spread ${o2.price} - ${o1.price} ${symbol}`)
+  }).catch(console.log)
+
+// Colour based on side
+bitmex.request('GET', '/order', { filter: '{"open": true}', reverse: true })
+  .then(orders => {
+    orders.map(({side,price,size,orderQty,symbol,transactTime}) => {
+      console.log(`open order ${side} ${orderQty} ${symbol} @ ${price} @ ${transactTime}`)
+    })
+  }).catch(console.log)
+
+//Colour based on sign of currentQty
+bitmex.request('GET', '/position', { filter: '{"isOpen": true}', reverse: true })
+  .then(positions => {
+    positions.map(({symbol,currentQty,avgEntryPrice,leverage,unrealisedPnl,unrealisedRoePcnt,realisedPnl,markPrice,liquidationPrice,commission}) => {
+      console.log(`open position ${symbol} ${currentQty} x${leverage} entry ${avgEntryPrice} mark ${markPrice} liq ${liquidationPrice} pnl ${unrealisedPnl}(${unrealisedRoePcnt*100}%)/${realisedPnl} comm ${commission}`)
+    })
+  }).catch(console.log)
