ea058cdf2c54ae4734ee4ee9c40799ec4e08cc73
sdclibbery <sdclibbery@googlemail.com>
2019-07-03 08:02:42 +0100

    tweak

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ const display = () => {
     const sellVol = Math.round(trades.filter(({side}) => side == 'Sell').map(({size}) => size).reduce((a,b)=>a+b, 0)/interval)
     const totalVol = buyVol + sellVol
     const maxBars = 8
-    const volLimit = 10000
+    const volLimit = 200000
     const buyBarSize = (totalVol<volLimit) ? Math.round(maxBars*buyVol/volLimit) : Math.round(2*maxBars*buyVol/totalVol)
     const sellBarSize = (totalVol<volLimit) ? Math.round(maxBars*sellVol/volLimit) : Math.round(2*maxBars*sellVol/totalVol)
     begin()(`BS ${interval}s:\t`).side('Buy', buyVol)('\t').side('Sell', sellVol)('\t')
